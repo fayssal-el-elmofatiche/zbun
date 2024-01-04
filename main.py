73ca0096c985c703c4b9c48717709c1c843b40dc
Fayssal El Mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-01-04 21:58:58 +0100

    changes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # imports
-import typer
 from typing import Annotated, List, Optional
 from rich import print
 import errno
 import os
 from copier import run_copy
 import pandas as pd
+import typer
 
 app = typer.Typer()
 
@@ -49,22 +49,6 @@ def delete(bundle_name, bundle_data):
 
     # return the bundle data
     return bundle_data
-
-def main(name: str = typer.Argument(..., help="The name of the person to greet.", metavar="NAME", show_default="World")):
-    print(f"Hello {name}!")
-
-@app.command()
-def ingest(bundle: Annotated[str, typer.Option(["-b", "--bundle"], help="The data bundle to ingest.", metavar="BUNDLE-NAME", show_default=True)] = DEFAULT_BUNDLE,
-           assets_version: Annotated[int, typer.Option("--assets-version", help="Version of the assets db to which to downgrade.")] = None,
-           show_progress: Annotated[bool, typer.Option(default=True, help="Print progress information to the terminal.")] = True):
-    """Ingest the data for the given bundle."""
-    bundles_module.ingest(
-        bundle,
-        os.environ,
-        pd.Timestamp.utcnow(),
-        assets_version,
-        show_progress,
-    )
 
 if __name__ == '__main__':
     app()
