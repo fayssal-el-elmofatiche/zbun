cf24d2151f320f52f71c8f04c2badba0d5129412
Fayssal El Mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-01-05 20:43:09 +0100

    latest changes

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from rich import print
 import errno
 import os
 from copier import run_copy
-import pandas as pd
 import typer
+from pathlib import Path
+import yaml
+import importlib
+import warnings
+warnings.filterwarnings("ignore")
 
 app = typer.Typer()
 
 # import zipline modules
 from zipline.data import bundles as bundles_module
 from zipline.utils.calendar_utils import get_calendar_names
+from zipline.data.bundles import register
 
 DEFAULT_BUNDLE = "quandl"
 
@@ -56,6 +61,56 @@ def calendars():
     """List all of the available trading calendars."""
     for name in sorted(get_calendar_names()):
         print(name)
+
+@app.command()
+def ingest(bundle_dir: str = typer.Option(".", "--dir", "-d", help="The directory of the bundle to ingest.")):
+    """Ingest data for a given bundle in current directory."""
+    p = Path(bundle_dir)
+
+    print(f"Bundle directory: {p.absolute()}")
+    zbundle_yaml_path = p.joinpath("zbundle.yaml")
+    print(f"zbundle.yaml path: {zbundle_yaml_path.absolute()}")
+
+    # check if folder contains zbundle.yaml
+    if not zbundle_yaml_path.is_file():
+        raise typer.Exit(f"No zbundle.yaml file found in given directory[{zbundle_yaml_path.absolute()}]. Please run `zbundle new` to create a new bundle, or `zbundle ingest` in a bundle directory.")
+    
+    # read bunlde name and calendar from zbundle.yaml
+    with open(zbundle_yaml_path, 'r') as f:
+        try:
+            zbundle_yaml = yaml.safe_load(f)
+            bundle_name = zbundle_yaml["bundle"]["name"]
+            bundle_calendar = zbundle_yaml["bundle"]["calendar"]
+            bundle_module = zbundle_yaml["bundle"]["module"]
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    print(f"Bundle name: {bundle_name}")
+    print(f"Bundle calendar: {bundle_calendar}")
+    print(f"Bundle module: {bundle_module}")
+
+    # cd to bundle directory
+    os.chdir(p)
+
+    # print content of directory
+    print(os.listdir())
+
+    # import bundle module
+    module = importlib.import_module(bundle_module)
+
+    # run test_load function from bundle module
+    module.test_module()
+
+    # register the bundle
+    print(f"Registering bundle {bundle_name} with calendar {bundle_calendar}")
+    register(bundle_name, module.process_data, calendar_name=bundle_calendar)
+
+    # ingest the bundle
+    print(f"Ingesting bundle {bundle_name}")
+    bundles_module.ingest(bundle_name)
+
+    # list bundles
+    #bundles()
 
 if __name__ == '__main__':
     app()
